2ad948a1ca9193abac515b5182b6b784b1ff2763
Askar Pulatov <master15994@gmail.com>
2024-06-07 13:40:10 +0500

    refactoring

--- components/game-field/game-field.jsx
+++ components/game-field/game-field.jsx
@@ -6,35 +6,61 @@ import CrosIcon from "../icons/cros-icon";
 
 const cells = new Array(19 * 19).fill(null)
 
+const actions = (
+  <div className="flex gap-2">
+    <UiButton variant="primary" size="lg">
+      Ничья
+    </UiButton>
+    <UiButton variant="outline" size="lg" >
+      Сдаться
+    </UiButton>
+  </div>
+)
+
 export default function GameField() {
   return (
     <div className="w-full min-h-80 p-4 mt-10  bg-white rounded-lg shadow-xl " >
-      <div className="flex align-center justify-between mb-5">
-
-        <div className="mr-auto">
-          <div className="flex items-center justify-center text-slate-900 font-bold">Ход:<CrosIcon /></div>
-          <div className=" flex items-center justify-center text-slate-900 text-xs leading-tight gap-2">Cледующий: <ZeroIcon /></div>
-        </div>
-
-        <div className="flex gap-2">
-          <UiButton variant="primary" size="lg">
-            Ничья
-          </UiButton>
-          <UiButton variant="outline" size="lg" >
-            Сдаться
-          </UiButton>
-        </div>
-
+      <div className="">
+        <GameMoveInfo actions={actions} />
       </div>
-
-      <div className="grid grid-cols-[repeat(19,_30px)] grid-rows-[repeat(19,_30px)] pt-px pl-px">
-        {cells.map((_, i) => (
-          <button key={i} className="border border-slate-200 -ml-px -mt-px flex items-center">
-            <ZeroIcon />
-          </button>
+      <GameGrid>
+        {cells.map((_, index) => (
+          <GameCell key={index}>
+          </GameCell>
         ))}
-      </div>
+      </GameGrid>
     </div>
   )
+}
 
+function GameCell({ children }) {
+  return (
+    <button
+      className="border border-slate-200 -ml-px -mt-px flex items-center">
+      {children}
+    </button>
+  )
+}
+
+function GameGrid({ children }) {
+  return (
+    <div className="grid grid-cols-[repeat(19,_30px)] grid-rows-[repeat(19,_30px)] pt-px pl-px">
+      {children}
+    </div>
+  )
+}
+
+
+function GameMoveInfo({ actions }) {
+  return (
+    <div className=" flex  align-center justify-between">
+      <div className="flex items-center justify-center text-slate-900 font-bold">
+        Ход:<CrosIcon />
+      </div>
+      <div className=" flex items-center justify-center text-slate-900 text-xs leading-tight gap-2">
+        Cледующий: <ZeroIcon />
+      </div>
+      {actions}
+    </div>
+  )
 }
